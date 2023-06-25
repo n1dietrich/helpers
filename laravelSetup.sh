60e7a6fabe6c298fea5719a88d263876358c61bb
N.Dietrich <77757135+n1dietrich@users.noreply.github.com>
2023-06-25 17:51:39 +0200

    Update laravelSetup.sh

--- laravelSetup.sh
+++ laravelSetup.sh
@@ -28,8 +28,8 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
 
 # PHP 8.2 und erforderliche Erweiterungen installieren
-sudo apt upgrade -y
-sudo apt install php-curl php8.2 php8.2-xml -y
+sudo apt upgrade -y.
+sudo apt install php-curl php8.2 php8.2-xml php8.2-mysql -y
 
 # Composer installieren
 sudo apt install composer -y
